af35e2724070676433080f8daeb463c02245db8c
londad99 <124159635+Londad99@users.noreply.github.com>
2025-10-15 00:04:35 -0500

    Setting up the project
    created role, user entities

---
+++ src/main/java/entity/Role.java
@@ -0,0 +1,19 @@
+package entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Short id;
+
+    @Column(nullable = false)
+    private String name;
+}
+
